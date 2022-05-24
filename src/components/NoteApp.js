19d40d7c3f78f871be38d552ed8eaf431e6db426
PazJal <30651575+PazJal@users.noreply.github.com>
2022-05-24 12:33:59 +0300

    refactor: move notelist to its own component

--- src/components/NoteApp.js
+++ src/components/NoteApp.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useReducer, useState} from 'react';
 
 import { notesReducer, ADD_NOTE, REMOVE_NOTE, POPULATE_NOTES } from '../reducers/notes';
-import Note from './Note';
+import NoteList from './NoteList';
 
 /**
  * 
@@ -65,9 +65,7 @@ import Note from './Note';
   return (
     <div className='p-5 max-w-xl space-y-3'>
       <h1  className='text-3xl font-bold underline text-blue-600'>Notes</h1>
-      { notes.map((note) => (
-        <Note key={note.title} note={note} removeNote={removeNote}/>
-      ))}
+      <NoteList notes={notes} removeNote={removeNote} />
       <p>Add Note</p>
       <form onSubmit={addNote}>
         <div className='flex-col flex max-w-xl space-y-3'>
